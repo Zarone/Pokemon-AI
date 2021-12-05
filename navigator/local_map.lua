9ac890b6224657bbf90b8201dc0f5eb7fe52a5c6
Zarone <redninjadolphin1@hotmail.com>
2021-12-05 10:32:02 -0500

    start on npc tracker

--- navigator/local_map.lua
+++ navigator/local_map.lua
@@ -3,9 +3,6 @@ local y = nil
 -- local z = nil
 local map_id = nil
 
--- local delta_time = 0
--- local wait_frames = 24
-
 -- 0 is players location -- red
 -- 1 is movable space -- white
 -- 2 is unmovable space -- black
@@ -31,71 +28,104 @@ function get_pos()
     return memory.readword(0x0224F912, 2), memory.readword(0x0224F91A), memory.readword(0x0224F916)
 end
 
+function reset_map()
+    local_map = {{0}}
+    map_x_start = 1
+    map_x_end = 1
+    map_y_start = 1
+    map_y_end = 1
+end
 
+function try_move(dir_x, dir_y) -- arguments are either (0, 1) or (1, 0)
+
+end
+
+function is_npc_at(grid_x, grid_y) -- x, y are indexes on local_map
+    -- y + yoffset = gridy
+    ingame_y = grid_y - y_offset
+    ingame_x = grid_x - x_offset
+
+    if memory.readbyteunsigned(0x023FFE09) == 0x00 then -- Not "2" ~ Not B2/W2
+        ows = md(0x02000024) + 0x34E04
+        game = 1
+    else
+        ows = md(0x02000024) + 0x36BE8
+        game = 2
+    end
+
+    mode = 2
+    editat = 0x08 + 5 * 0x14 + ows
+    count = memory.readbyteunsigned(ows + 3 + mode)
+
+    for i = 0, count-1
+    do
+        x_addr = editat + 227 * 0x24 - 2 + 16 * (14+16*i)
+        -- y_addr = x_addr + 8
+        print(memory.readword(x_addr), memory.readword(x_addr+8))
+        -- gui.text(1, i*20+60, sf("%d", memory.readword(editat + 227 * 0x24 - 2 + 16 * (14+16*i))))
+        -- gui.text(1, i*20+70, sf("%d", memory.readword(editat + 227 * 0x24 + 6 + 16 * (14+16*i))))
+    end
+end
 
 while true do
-    -- delta_time = delta_time + 1
 
     old_x = x
     old_y = y
 
-    -- if delta_time % wait_frames == 0 then
+    x, y = get_pos()
+    if map_id ~= get_map() -- if the map has changed
+    then
+        print("new map")
+
+        -- first check to see if the map is already stored somewhere
+        -- if not make a new offset x and y
+        reset_map()
+        y_offset = -y
+        x_offset = -x
+
+        map_id = get_map()
+    elseif x ~= old_x or y ~= old_y then -- if the user moved
+        -- print("moved")
+
         x, y = get_pos()
-        if map_id ~= get_map() -- if the map has changed
-        then
-            print("new map")
-            
-            -- first check to see if the map is already stored somewhere
-            -- if not make a new offset x and y
-            y_offset = -y
-            x_offset = -x
-            
-            map_id = get_map()
-        elseif x ~= old_x or y ~= old_y then
-            -- print("moved")
 
-            x, y = get_pos()
-
-            -- set the last location to movable space
-            if old_x ~= nil then
-                local_map[old_y+y_offset+1][old_x+x_offset+1] = 1
-            end
-            
-            -- expand the map
-            if (y+y_offset+1 < map_y_start) then
-                map_y_start = y+y_offset+1 
-            elseif (y+y_offset+1 > map_y_end) then
-                map_y_end = y+y_offset+1 
-            end
-
-            if (x+x_offset+1 < map_x_start) then
-                map_x_start = x+x_offset+1 
-            elseif (x+x_offset+1 > map_x_end) then
-                map_x_end = x+x_offset+1 
-            end
-
-
-            if (y ~= old_y) then -- if the y changed
-                if (local_map[y+y_offset+1] == nil) then -- if the new row is nil
-                    local_map[y+y_offset+1] = {}
-                end
-                local_map[y+y_offset+1][x+x_offset+1] = 0
-            end
-
-            if (x ~= old_x) then
-                local_map[y+y_offset+1][x+x_offset+1] = 0
-            end
+        -- set the last location to movable space
+        if old_x ~= nil then
+            local_map[old_y + y_offset + 1][old_x + x_offset + 1] = 1
         end
-        -- delta_time = 0
-    -- end
+
+        -- expand the map
+        if (y + y_offset + 1 < map_y_start) then
+            map_y_start = y + y_offset + 1
+        elseif (y + y_offset + 1 > map_y_end) then
+            map_y_end = y + y_offset + 1
+        end
+
+        if (x + x_offset + 1 < map_x_start) then
+            map_x_start = x + x_offset + 1
+        elseif (x + x_offset + 1 > map_x_end) then
+            map_x_end = x + x_offset + 1
+        end
+
+        if (y ~= old_y) then -- if the y changed
+            if (local_map[y + y_offset + 1] == nil) then -- if the new row is nil
+                local_map[y + y_offset + 1] = {}
+            end
+            local_map[y + y_offset + 1][x + x_offset + 1] = 0
+        end
+
+        if (x ~= old_x) then
+            local_map[y + y_offset + 1][x + x_offset + 1] = 0
+        end
+    end
 
     if debug_map then
         for i = map_y_start, map_y_end do -- goes through all the rows 
             for j = map_x_start, map_x_end do -- goes through all the columns in the row
-                
+
                 node_type = local_map[i][j]
-                color = { }
-                
+                color = {}
+
                 if (node_type ~= nil) then
                     if node_type == 0 then
                         color = {255, 0, 0, 255}
@@ -108,11 +138,11 @@ while true do
                     elseif node_type == 4 then
                         color = {0, 255, 0, 255}
                     end
-                    
-                    x_alt = 15*(x+x_offset - (j-1)) 
-                    y_alt = 15*(y+y_offset - (i-1)) 
-                    gui.box(123-x_alt, 90-y_alt, 133-x_alt, 100-y_alt, color, {255, 255, 255, 255})
-    
+
+                    x_alt = 15 * (x + x_offset - (j - 1))
+                    y_alt = 15 * (y + y_offset - (i - 1))
+                    gui.box(123 - x_alt, 90 - y_alt, 133 - x_alt, 100 - y_alt, color, {255, 255, 255, 255})
+
                 end
             end
         end
