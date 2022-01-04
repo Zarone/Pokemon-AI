641bc7e18c71d90f2db6cd17f577b532ed4705e9
Zachary Alfano <zacharyalfano2@gmail.com>
2022-01-04 13:01:03 -0500

    work on backprop net

--- battle_ai/backprop_ai/network.py
+++ battle_ai/backprop_ai/network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 class Network():
@@ -17,18 +18,55 @@ class Network():
                         for x, y in zip(self.sizes[:-1], self.sizes[1:])]
 
     # Stochastic Gradient Descent
-    def SGD(self, training_data, epochs, per_batch): 
+    def SGD(self, training_data, epochs, per_batch, learning_rate, activation):
         # training_data is list of elements like (x, y),
         # where x is the input and y is the correct output
-        
-        # for each epoch
-            # shuffle training data
-            # get array of all batches
-                # each batch is of a small section of the training data, 
-                # each one with size "per_batch"
-            # call self.train_batch(mini_batch)
+
+        for i in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[j:j+per_batch]
+                            for j in range(0, len(training_data), per_batch)]
+            for batch in mini_batches:
+                self.train_batch(batch, learning_rate, activation)
         pass
 
-    def train_batch(self, mini_batch):
-        pass
-        
+    def train_batch(self, mini_batch, learning_rate, activation):
+        x = np.asarray([_x for _x, _y in mini_batch]).transpose()
+        y = np.asarray([_y for _x, _y in mini_batch]).transpose()
+
+        nabla_b, nabla_w = None
+        if activation == "relu":
+            self.backprop(x, y)
+        elif activation == "lrelu":
+            self.backprop(x, y)
+        elif activation == "tanh":
+            self.backprop(x, y)
+
+        self.weights = [w-(learning_rate/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+
+
+    def backprop(self, x, y, activation):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        zs = []
+        activations = [x]
+        activation = x
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = activation(z)
+            activations.append(activation)
+
+net = Network([4, 4])
+net.SGD(
+    [
+        [np.array([0, 0, 1, 2, 3]), np.array([0, 0, 0, 0, 0, 1])],
+        [np.array([0, 0, 1, 2, 3]), np.array([0, 0, 0, 0, 0, 1])],
+        [np.array([0, 0, 1, 2, 3]), np.array([0, 0, 0, 0, 0, 1])],
+        [np.array([0, 0, 1, 2, 3]), np.array([0, 0, 0, 0, 0, 1])],
+        [np.array([0, 0, 1, 2, 3]), np.array([0, 0, 0, 0, 0, 1])],
+    ],
+    1, 5, 0.03
+)
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Overall Structure
+
+- A system which is responsible for reading data from game RAM
+- A system which takes in that data and outputs an option
