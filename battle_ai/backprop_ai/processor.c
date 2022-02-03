68228fc7b623519bdf0cf530b3f935bfe9579a0e
Zachary Alfano <zacharyalfano2@gmail.com>
2022-02-02 21:15:34 -0500

    alter compiler settings

--- battle_ai/backprop_ai/processor.c
+++ battle_ai/backprop_ai/processor.c
@@ -1,12 +1,40 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "luaconf.h"
+#include "mpack/mpack.h"
 #include <stdio.h>
+
+mpack_tree_t getTree(int *error){
+  mpack_tree_t tree;
+  mpack_tree_init_filename(&tree, "./battle_ai/backprop_ai/weights.txt", 100);
+  mpack_tree_parse(&tree);
+  mpack_node_t root = mpack_tree_root(&tree);
+
+  // extract the example data on the msgpack homepage
+  bool compact = mpack_node_bool(mpack_node_map_cstr(root, "compact"));
+  int schema = mpack_node_i32(mpack_node_map_cstr(root, "schema"));
+
+  // clean up and check for errors
+  return tree;
+  // if (mpack_tree_destroy(&tree) != mpack_ok) {
+  //     fprintf(stderr, "An error occurred decoding the data!\n");
+  //     *error = 1;
+  // }
+}
+
+
 
 int get_move(lua_State *L){
     
     // lua should pass in zero arguments for this
     lua_settop(L, 0);
+
+    int error;
+    mpack_tree_t tree = getTree(&error);
+    if (error != 0){
+      printf("success");
+    }
+
     lua_pushstring(L, "value pushed from get_move()");
     return 1;
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd battle_ai ;
 cd backprop_ai ;
-cc -bundle -undefined dynamic_lookup -o "./build/processor.so" processor.c -Ilua_src;
+cc -bundle -undefined dynamic_lookup -o "./build/processor.so" "./mpack/mpack.c" processor.c -Ilua_src -Ihelper;
 cd ../../ ;
 ./battle_ai/showdown/build ;
 lua ./battle_ai/battle_manager.lua
