b62bf2d3fb070b483718bfcdaa8f6e56ef8c4696
Zarone <redninjadolphin1@hotmail.com>
2022-04-24 10:14:18 -0400

    altered global pathfinder

--- navigator/global_map.lua
+++ navigator/global_map.lua
@@ -16,13 +16,16 @@ end
 
 gmd.go_to_map = function(current_map, current_x, current_y, to_map) -- returns path
     -- print("go to map: start")
-    -- Dijkra's Algorithm, because there's no heuristic
+    -- Dijkra's inspired algorithm, because there's no heuristic
 
     -- Create a queue
-    queue = {}
+    local queue = {}
 
     -- create a list to track visited nodes
-    visited_nodes = {}
+    local visited_nodes = {}
+
+    local best_path = nil
+    local best_path_length = 1E10
 
     -- add current item to list {current_map, current_x, current_y, 0, {}}
     -- the zero is for heuristic distance
@@ -33,26 +36,23 @@ gmd.go_to_map = function(current_map, current_x, current_y, to_map) -- returns p
     while #queue > 0 do
         -- find item at start of queue
         -- store item as variable
-        checking_node = queue[1]
-        -- print("current_node: ", checking_node)
+        local checking_node = queue[1]
 
         -- pop it
         table.remove(queue, 1)
 
         -- find all places player could go from current item
-        all_neighbors = {}
+        local all_neighbors = {}
 
         -- if the map object currently has data on this new map
         if gmd.map[checking_node[1]] ~= nil then
             for _, neighbor in pairs(gmd.map[checking_node[1]]) do
                 
                 -- checks if visited_nodes has any element with the same map as this neighbor
-                map_in_visited = visited_nodes[neighbor[1]] ~= nil
+                local map_in_visited = visited_nodes[neighbor[1]] ~= nil
                 
-                neighbor_has_been_visited = false
+                local neighbor_has_been_visited = false
                 for _, visited_node in pairs(visited_nodes) do
-                    -- print("node_has_been_visited: ", neighbor_has_been_visited)
-                    -- print(visited_node[1][1], neighbor[4], visited_node[1][2], neighbor[5])
                     if visited_node[1][1] == neighbor[4] and visited_node[1][2] == neighbor[5] then
                         neighbor_has_been_visited = true
                         break
@@ -62,40 +62,44 @@ gmd.go_to_map = function(current_map, current_x, current_y, to_map) -- returns p
                 if not neighbor_has_been_visited then
     
                     -- print(checking_node, neighbor)
-                    cost_to_neighbor = distance(checking_node[2], checking_node[3], neighbor[2], neighbor[3])
-        
-                    -- inserts the cheapest neighbors furthest at the start 
-                    i = 1
-                    while i < #all_neighbors + 1 and cost_to_neighbor > all_neighbors[i][4] do
-                        i = i + 1
-                    end
+                    local cost_to_neighbor = distance(checking_node[2], checking_node[3], neighbor[2], neighbor[3])
     
-                    new_path = {}
+                    local new_path = {}
     
-                    last_path = unpack(checking_node[5])
+                    local last_path = unpack(checking_node[5])
     
                     if (last_path ~= nil) then
                         new_path = {last_path, {neighbor[2], neighbor[3]}}
                     else
                         new_path = {{neighbor[2], neighbor[3]}}
                     end
+
+                    local this_neighbor_path_length = checking_node[4] + cost_to_neighbor
     
                     if neighbor[1] == to_map then 
-                        return new_path
+                        if (this_neighbor_path_length < best_path_length) then
+                            best_path_length = checking_node[4] + cost_to_neighbor
+                            best_path = new_path
+                        end
                     end
     
-                    -- {to_map, to_x, to_y, cost, path}
-                    -- print(neighbor[4], neighbor[5])
-                    all_neighbors[i] = {neighbor[1], neighbor[4], neighbor[5], checking_node[4] + cost_to_neighbor, new_path}
+
+                    -- this if statement is triggers if this neighbor would be the longest length in the queue
+
+                    if #queue == 0 or this_neighbor_path_length >= queue[#queue][4] then
+                        table.insert(queue, {neighbor[1], neighbor[4], neighbor[5], this_neighbor_path_length, new_path})
+                    else
+                        for i, v in ipairs(queue) do
+                            if (this_neighbor_path_length < v[4]) then
+                                table.insert(queue, i, {neighbor[1], neighbor[4], neighbor[5], this_neighbor_path_length, new_path})
+                                break
+                            end
+                        end
+                    end
+
                 end
             end
-    
-            for _, sorted_neighbor in pairs(all_neighbors) do
-                table.insert(queue, sorted_neighbor)
-            end
         end
-
-        -- print("neighbors: ", all_neighbors)
         
         -- add item to list of visited nodes
         if visited_nodes[checking_node[1]] == nil then
@@ -103,16 +107,14 @@ gmd.go_to_map = function(current_map, current_x, current_y, to_map) -- returns p
         end
         table.insert(visited_nodes[checking_node[1]], {checking_node[2], checking_node[3]})
         
-        -- print("visited nodes: ", visited_nodes)
-        -- print("\n\n")
     end
-    return false
+    return best_path
 
 end
 
 -- gmd.map[1] = {{2, 3, 4, 2, 2}, {4, 3, 1, 4, 6}}
 -- gmd.map[2] = {{3, 1, 1, 4, 1}}
 
--- print(gmd.go_to_map(1, 3, 3, 3))
+-- gmd.go_to_map(1, 3, 3, 3)
 
 return gmd
