96f12758f67fe467d378f5abe7256434cf6ef919
Zarone <redninjadolphin1@hotmail.com>
2022-06-15 17:52:30 -0400

    made sure player goes to accumula pokemon center

--- navigator/goals.lua
+++ navigator/goals.lua
@@ -22,36 +22,42 @@ goal_manager = {
         {0, {317, 788, 672}, {1, 1}}, -- (make sure player walks in right direction when wander to Accumula town)
         
         {0, {317, 789, 672}, {1, 1}}, -- go to Accumula town
-        {0, {397, 796, 659}, {1, 1}},
-        {2, {{up = true}, 50}, {1, 1}},
-        {2, {{A = true}, 100}, {1, 1}},
-        {2, {{up = true}, 20}, {1, 1}},
+        {0, {397, 796, 659}, {1, 1}}, -- go to talk to professor in front of pokemon center
+        {2, {{up = true}, 50}, {1, 1}}, -- correct orientation
+        {2, {{A = true}, 100}, {1, 1}}, -- start dialogue
+        {2, {{up = true}, 20}, {1, 1}}, -- correct orienation in pokemon center
         
-        {2, {{A = true}, 1}, {1, 1}},
-        {0, {397, 786, 658}, {1, 1}},        
+        {2, {{A = true}, 1}, {1, 1}}, -- start dialogue in pokemon center
+        {0, {397, 797, 658}, {1, 1}}, -- go in front of Accumula town pokemon center
+        {0, {397, 795, 658}, {1, 1}},
+        {0, {397, 796, 659}, {1, 1}},
+        {0, {397, 796, 658}, {1, 1}},
+        {0, {398, 7, 19}, {1, 1}}, -- go into Accumula town pokemon center
+
+        {0, {397, 786, 658}, {1, 1}}, -- go to the team plasma monologue
         {0, {319, 786, 613}, {1, 1}},
         {0, {319, 786, 608}, {1, 1}},
         {2, {{up = true}, 50}, {1, 1}},
-        
         {0, {6, 782, 588}, {1, 1}}, -- in Stration City
+
         {0, {6, 780, 588}, {1, 1}},
         {0, {6, 781, 588}, {1, 1}},
         {0, {8, 7, 19}, {1, 1}}, -- Stration city pokemon center
         {0, {6, 774, 587}, {1, 1}}, 
-
         {0, {15, 8, 2}, {1, 1}},
+        
         {2, {{right = true}, 20}, {1, 1}},
         {2, {{A = true}, 1}, {1, 1}},
         {0, {6, 788, 588}, {1, 1}},
         {2, {{up = true}, 20}, {1, 1}},
-        
         {2, {{A = true}, 1}, {1, 1}},
+
         {0, {6, 788, 587}, {1, 1}},
         {0, {7, 15, 36}, {1, 1}},
         {0, {7, 9, 26}, {1, 1}},
         {0, {7, 12, 16}, {1, 1}},
-        
         {0, {7, 12, 4}, {1, 1}},
+        
         {2, {{A = true}, 1}, {1, 1}},
     }
 }
